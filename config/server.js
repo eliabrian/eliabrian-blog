251a90b352da617012450dc42bbd6892238e0f03
eliabrian <eliabrian69@gmail.com>
2020-10-01 00:56:05 +0700

    db

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '86c5d1cfe401260697fe593c78d588f6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '01bf1ee3-2ecf-49e3-b10c-bb42803a4bab'
+};
